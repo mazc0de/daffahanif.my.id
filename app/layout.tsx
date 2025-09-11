355cd67cf86319f84d18097878e4b9f0e1d007b0
Daffa Hanifisyafiq <dandroid36@gmail.com>
2025-09-12 00:24:40 +0700

    refactor: update fonts and typography

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Inter, Rethink_Sans } from 'next/font/google'
 import './globals.css'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin']
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin']
-})
+const inter = Inter({ subsets: ['latin'], variable: '--_inter' })
+const rethink = Rethink_Sans({ subsets: ['latin'], variable: '--_rethink' })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,16 +12,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
-    <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang='en' className={`${rethink.variable} ${inter.variable}`}>
+      <body className='font-body antialiased'>{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      <h1 className='font-semibold'>Hello World</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime harum,
+        perferendis eius, at soluta fuga a iste eum quidem maiores eligendi!
+        Quisquam, aut. Ratione, dolore quod aut rem veniam libero?
+      </p>
+    </div>
+  )
 }
 
 export default Home
